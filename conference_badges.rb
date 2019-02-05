7842e5cd4e08372bbb1acf8abe73da91d13c7d47
Nicholas Dover <dovern42@gmail.com>
2019-02-05 05:17:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ end
 
 def assign_rooms(names)
   array = []
-  names.each_with_index{|name, index| array.push("Hello, #{name}! You'll be assigned to room #{index}!")}
+  names.each_with_index{|name, index| array.push("Hello, #{name}! You'll be assigned to room #{index+1}!")}
   array
 end
